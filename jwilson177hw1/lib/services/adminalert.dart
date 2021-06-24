974ef539eb336bc57e29982eb47331ccfc8443ba
jmw11x <jmw11x@gmail.com>
2021-06-23 23:56:32 -0400

    finalpatch_timestampadd

--- jwilson177hw1/lib/services/adminalert.dart
+++ jwilson177hw1/lib/services/adminalert.dart
@@ -1,3 +1,4 @@
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AdminAlert {
@@ -9,7 +10,8 @@ class AdminAlert {
     await messages.getDocuments().then((message) {
       message.documents.forEach((value) {
         if (value['message'] != null) {
-          // print(value['message']);
+          // print();
+
           msgs = msgs + (value['message']) + "\n";
         }
       });
@@ -23,6 +25,7 @@ class AdminAlert {
   Future addAdminMessage(String message) async {
     await messages.document().setData({
       'message': message,
+      'datetime': DateTime.now().toString(),
     });
   }
 }
